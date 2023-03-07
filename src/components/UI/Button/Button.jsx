5a4dd93e638cce8999b99ebc63c04760413e970b
Nikola Bogicevic <nbogicevic2@gmail.com>
2023-03-07 11:10:48 -0500

    using css modules

--- src/components/UI/Button/Button.jsx
+++ src/components/UI/Button/Button.jsx
@@ -1,43 +1,47 @@
-import styled from "styled-components";
+import React from "react";
 
-const Button = styled.button`
-	display: inline-block;
-	width: 100%;
-	padding: 0.8rem 1rem;
-	border: 2px solid #eaba6b;
-	border-radius: 30px;
-	color: #000;
-	font: inherit;
-	font-weight: 700;
-	text-decoration: none;
-	background-color: #eaba6b;
-	cursor: pointer;
+import styles from "./Button.module.scss";
 
-	@media (min-width: 768px) {
-		width: auto;
-	}
+// import styled from "styled-components";
 
-	&:hover,
-	&:active,
-	&:focus {
-		color: #eaba6b;
-		border-color: #eaba6b;
-		background-color: #0075a2;
-		outline-color: #eaba6b;
-	}
+// const Button = styled.button`
+// 	display: inline-block;
+// 	width: 100%;
+// 	padding: 0.8rem 1rem;
+// 	border: 2px solid #eaba6b;
+// 	border-radius: 30px;
+// 	color: #000;
+// 	font: inherit;
+// 	font-weight: 700;
+// 	text-decoration: none;
+// 	background-color: #eaba6b;
+// 	cursor: pointer;
 
-	&:focus {
-		outline: 2px solid webkit-focus-ring-color;
-		outline-offset: -2px;
-	}
-`;
+// 	@media (min-width: 768px) {
+// 		width: auto;
+// 	}
 
-// const Button = (props) => {
-// 	return (
-// 		<button type={props.type} className="button" onClick={props.onClick}>
-// 			{props.children}
-// 		</button>
-// 	);
-// };
+// 	&:hover,
+// 	&:active,
+// 	&:focus {
+// 		color: #eaba6b;
+// 		border-color: #eaba6b;
+// 		background-color: #0075a2;
+// 		outline-color: #eaba6b;
+// 	}
+
+// 	&:focus {
+// 		outline: 2px solid webkit-focus-ring-color;
+// 		outline-offset: -2px;
+// 	}
+// `;
+
+const Button = (props) => {
+	return (
+		<button type={props.type} className={styles.button} onClick={props.onClick}>
+			{props.children}
+		</button>
+	);
+};
 
 export default Button;

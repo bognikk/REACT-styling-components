a6ea6f47ef32ef9a76e5fe13678be6aab110ca75
Nikola Bogicevic <nbogicevic2@gmail.com>
2023-03-07 10:59:46 -0500

    dynamic props on styled comp

--- src/components/CourseGoals/CourseInput/CourseInput.jsx
+++ src/components/CourseGoals/CourseInput/CourseInput.jsx
@@ -1,7 +1,51 @@
 import React, { useState } from "react";
+import styled from "styled-components";
 
 import Button from "../../UI/Button/Button";
 import "./CourseInput.scss";
+
+const FormControl = styled.div`
+	display: flex;
+	flex-wrap: wrap;
+	flex-direction: column;
+	width: 100%;
+	margin-bottom: 1rem;
+	text-align: left;
+	font-weight: 700;
+
+	& label {
+		display: block;
+		margin-bottom: 0.5rem;
+		font-weight: bold;
+		display: block;
+		margin-bottom: 0.5rem;
+		font-weight: 700;
+		color: ${(props) => (props.invalid ? "salmon" : "#000")};
+	}
+
+	& input {
+		display: block;
+		max-width: 100%;
+		width: 100%;
+		margin: 0.5rem 0;
+		padding: 0.5rem;
+		border-radius: 5px;
+		border: 1px solid ${(props) => (props.invalid ? "salmon" : "#ccc")};
+		background: ${(props) => (props.invalid ? "salmon" : "")};
+		color: #000;
+		font: inherit;
+
+		&:focus {
+			border-color: #eaba6b;
+			background-color: #e6c99c;
+			outline-color: #eaba6b;
+		}
+
+		&:focus-visible {
+			outline-color: #eaba6b !important;
+		}
+	}
+`;
 
 const CourseInput = (props) => {
 	const [enteredValue, setEnteredValue] = useState("");
@@ -26,14 +70,14 @@ const CourseInput = (props) => {
 
 	return (
 		<form onSubmit={formSubmitHandler}>
-			<div className={`form-control ${!isValid ? "invalid" : ""}`}>
+			<FormControl invalid={!isValid}>
 				<label>Course Goal</label>
 				<input
 					type="text"
 					onChange={goalInputChangeHandler}
 					value={enteredValue}
 				/>
-			</div>
+			</FormControl>
 			<Button type="submit">Add Goal</Button>
 		</form>
 	);
